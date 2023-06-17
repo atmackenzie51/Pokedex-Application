6439b05ef5f1cfa938522d7ad1b0aa45d6b45ff1
atmackenzie51 <126356439+atmackenzie51@users.noreply.github.com>
2023-06-17 14:42:51 -0400

    added 6 starter pokemon to application
    removed the initial assignment, add the first 6 starter pokemon to the pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,45 @@
-alert('Hello, World!');
-
-let favoriteFood = 'Rice';
-document.write(favoriteFood);
+// Database of Gen 1 and 2 starter pokemon with a hatch steps
+let pokemonList = [
+    {
+        name: 'Bulbasaur',
+        pokedexID: 1,
+        height: 0.7,
+        type: ['grass', 'poison'], 
+        hatchSteps: 5100
+    },
+    {
+        name: 'Charmander',
+        pokedexID: 4,
+        height: 0.6,
+        type: ['fire'],
+        hatchSteps: 5100
+    },
+    {
+        name: 'Squirtle',
+        pokedexID: 7,
+        height: 0.5,
+        type: ['water'],
+        hatchSteps: 5100
+    },
+    {
+        name: 'Chikorita',
+        pokedexID: 152,
+        height: 0.9,
+        type: ['grass'], 
+        hatchSteps: 5100
+    },
+    {
+        name: 'Cyndaquil',
+        pokedexID: 155,
+        height: 0.5,
+        type: ['fire'],
+        hatchSteps: 5100
+    },
+    {
+        name: 'Totodile',
+        pokedexID: 158,
+        height: 0.6,
+        type: ['water'],
+        hatchSteps: 5100
+    },
+];
