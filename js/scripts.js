ea27602b073964c83ccb083a90bda75755a33824
atmackenzie51 <126356439+atmackenzie51@users.noreply.github.com>
2023-06-19 18:39:54 -0400

    added an IIFE to array, updated functions to match IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,59 +1,78 @@
-// Database of Gen 1 and 2 starter pokemon with a hatch steps
-let pokemonList = [
-  {
-    name: "Bulbasaur",
-    pokedexID: 1,
-    height: 0.7,
-    type: ["grass", "poison"],
-    hatchSteps: 5100,
-  },
-  {
-    name: "Charmander",
-    pokedexID: 4,
-    height: 0.6,
-    type: ["fire"],
-    hatchSteps: 5100,
-  },
-  {
-    name: "Squirtle",
-    pokedexID: 7,
-    height: 0.5,
-    type: ["water"],
-    hatchSteps: 5100,
-  },
-  {
-    name: "Chikorita",
-    pokedexID: 152,
-    height: 0.9,
-    type: ["grass"],
-    hatchSteps: 5100,
-  },
-  {
-    name: "Cyndaquil",
-    pokedexID: 155,
-    height: 0.5,
-    type: ["fire"],
-    hatchSteps: 5100,
-  },
-  {
-    name: "Totodile",
-    pokedexID: 158,
-    height: 0.6,
-    type: ["water"],
-    hatchSteps: 5100,
-  },
-];
+// Setting a IIFE for Database of Gen 1 and 2 starter pokemon with a hatch steps
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: "Bulbasaur",
+      pokedexID: 1,
+      height: 0.7,
+      type: ["grass", "poison"],
+      hatchSteps: 5100,
+    },
+    {
+      name: "Charmander",
+      pokedexID: 4,
+      height: 0.6,
+      type: ["fire"],
+      hatchSteps: 5100,
+    },
+    {
+      name: "Squirtle",
+      pokedexID: 7,
+      height: 0.5,
+      type: ["water"],
+      hatchSteps: 5100,
+    },
+    {
+      name: "Chikorita",
+      pokedexID: 152,
+      height: 0.9,
+      type: ["grass"],
+      hatchSteps: 5100,
+    },
+    {
+      name: "Cyndaquil",
+      pokedexID: 155,
+      height: 0.5,
+      type: ["fire"],
+      hatchSteps: 5100,
+    },
+    {
+      name: "Totodile",
+      pokedexID: 158,
+      height: 0.6,
+      type: ["water"],
+      hatchSteps: 5100,
+    },
+  ];
 
-pokemonList.forEach(function(pokemon){
-  if (pokemon.height > 0.8) {
-    document.write (
-      pokemon.name + `: (Height: ${pokemon.height})` + " - WOW, that\'s a big one!"
-    )
-  }else{
-
-  document.write(
-    pokemon.name + `: (Height: ${pokemon.height})`
-  );
+  //two functions add and getAll within the IIFE
+  function add(pokemon) {
+    if (typeof pokemon === "object"){
+      pokemonList.push(pokemon);
+    }
   }
-  document.write('<br> <br>')
-});
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  // creation of objects with the IIFE
+  return {
+    add: add,
+    getAll: getAll,
+  };
+})();
+
+function pokemonInfo(pokemon) {
+  if (pokemon.height > 0.8) {
+    document.write(
+      pokemon.name +
+        `: (Height: ${pokemon.height})` + " - WOW, that's a big one!"
+    );
+  } else {
+    document.write(pokemon.name + `: (Height: ${pokemon.height})`);
+  }
+  document.write("<br> <br>");
+}
+
+pokemonRepository.getAll().forEach(pokemonInfo);
